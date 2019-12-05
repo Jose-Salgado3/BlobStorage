5c6d3b877965ffa76a2f804591d4993e7ef61498
Jose Salgado <Josalgado03@gmail.com>
2019-12-05 11:35:34 -0800

    Added DeleteBlobData and GenerateFIleName method

--- ServiceLayer/BlobStorageService.cs
+++ ServiceLayer/BlobStorageService.cs
@@ -1,6 +1,9 @@
-﻿using ServiceLayer.AppConfig;
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using ServiceLayer.AppConfig;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,6 +31,34 @@ namespace ServiceLayer
             {
                 throw (ex);
             }
+        }
+
+        public async void DeleteBlobData(string fileUrl)
+        {
+            Uri uriObj = new Uri(fileUrl);
+            string BlobName = Path.GetFileName(uriObj.LocalPath);
+
+            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(accessKey);
+            CloudBlobClient cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+            string strContainerName = "uploads";
+            CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference(strContainerName);
+
+            string pathPrefix = DateTime.Now.ToUniversalTime().ToString("yyyy-MM-dd") + "/";
+            CloudBlobDirectory blobDirectory = cloudBlobContainer.GetDirectoryReference(pathPrefix);
+            // get block blob reference
+            CloudBlockBlob blockBlob = blobDirectory.GetBlockBlobReference(BlobName);
+
+            //delete blob from container
+            await blockBlob.DeleteAsync();
+        }
+
+        private string GenerateFileName(string fileName)
+        {
+            string strFileName = string.Empty;
+            string[] strName = fileName.Split('.');
+            strFileName = DateTime.Now.ToUniversalTime().ToString("yyyy-MM-dd")
+                + "/" + DateTime.Now.ToUniversalTime().ToString("yyyyMMdd//THHmmssfff")
+                + "." + strName[strName.Length - 1];
         }
     }
 }
