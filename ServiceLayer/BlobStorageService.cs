6ea49fa3e7ad9cb0c0ab30b514ec372eafda8c8e
Jose Salgado <Josalgado03@gmail.com>
2019-12-05 11:22:31 -0800

    Added BlobStorageService file responsible for handling the files between the Blob storage and the Application. Added UploadFileToBlob

---
+++ ServiceLayer/BlobStorageService.cs
@@ -0,0 +1,33 @@
+﻿using ServiceLayer.AppConfig;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer
+{
+    public class BlobStorageService
+    {
+        string accessKey = string.Empty;
+
+        public BlobStorageService()
+        {
+            this.accessKey = AppConfiguration.GetConfiguration("AccessKey");
+        }
+
+        public string UploadFileToBlob(string strFileName, byte[] fileData, string fileMimeType)
+        {
+            try
+            {
+                var _task = Task.Run(() => this.UploadFileToBlob(strFileName, fileData, fileMimeType));
+                _task.Wait();
+                string fileUrl = _task.Result;
+                return fileUrl;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+    }
+}
